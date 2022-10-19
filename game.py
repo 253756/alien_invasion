f11b975b3f7855bb4c11f2af9cc83ac6a76a28da
253756 <m253756@usna.edu>
2022-10-19 14:12:26 -0400

    new updates

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -24,17 +25,8 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            #watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            #redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            #make the most recently drawn screen visible.
-            pygame.display.flip()
+            gf.check_events()
+            gf.update_screen(ai_settings,screen,ship)
 
 if __name__ == '__main__':
     #Make a game instance and run the game.
---
+++ game_functions.py
@@ -0,0 +1,19 @@
+import sys
+
+import pygame
+
+def check_events():
+    """Respond to key presses and mouse events."""
+    # watch for keyboard and mouse events.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+    """Update images on the screen and flip to the new screen"""
+    # redraw the screen during each pass through the loop
+    self.screen.fill(self.settings.bg_color)
+    self.ship.blitme()
+
+    # make the most recently drawn screen visible.
+    pygame.display.flip()
